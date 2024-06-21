6741c5511357fbd7314918505c26639b0a9dd12e
Thomas Gransden <thomas.gransden@petsathome.co.uk>
2024-06-21 20:04:11 +0100

    Updates

---
+++ blog/welcome.md
@@ -0,0 +1,15 @@
+---
+title: Welcome
+description: This is my first post on Docusaurus.
+authors: tomg
+slug: welcome-docusaurus-v2
+tags: []
+image: https://i.imgur.com/mErPwqL.png
+hide_table_of_contents: false
+---
+
+Hi! I'm Tom, welcome to my blog. I will mainly be posting my own notes about React Native.
+
+<!-- truncate -->
+
+Hope you find them useful.
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,20 +3,15 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
+  title: 'Thomas Gransden',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: 'https://tomgransden.github.io', // Your website URL
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'tomgransden.github.io',
+  organizationName: 'tomgransden',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
